93110a4bb380c0a33982613344be26175335154f
Reilly Miller <reillyjmiller31@gmail.com>
2023-06-11 15:18:34 -0600

    update routes to include users

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v0 do
       resources :forecast, only: [:index]
-
+      post :users, to: 'users#create'
+      post :sessions, to: 'sessions#create'
     end
   end
 end
