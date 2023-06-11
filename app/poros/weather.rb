52c8881796a6c1712149ec13c50b9864fcae3c48
Reilly Miller <reillyjmiller31@gmail.com>
2023-06-11 15:18:07 -0600

    create weather poros

--- app/poros/weather.rb
+++ app/poros/weather.rb
@@ -1,5 +1,14 @@
 class Weather
-  def initialize(data)
-    
+  attr_reader :current_weather,
+              :daily_weather,
+              :hourly_weather,
+              :id,
+              :type
+  def initialize(current_weather, daily_weather, hourly_weather)
+    @id = 'null'
+    @type = 'forecast'
+    @current_weather = current_weather
+    @daily_weather = daily_weather
+    @hourly_weather = hourly_weather
   end
 end
