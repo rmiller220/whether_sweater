0334c57200be3346343534eb9769292b8c9598e0
Reilly Miller <reillyjmiller31@gmail.com>
2023-06-12 15:48:33 -0600

    add another validation and test for model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   validates_presence_of :password
+  validates_presence_of :password_confirmation
   has_secure_password
+
+  before_create :generate_api_key
+
+  private
+  def generate_api_key
+    self.api_key = SecureRandom.hex(16)
+  end
 end
