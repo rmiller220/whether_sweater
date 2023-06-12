965a8a0958e8eab433d7dd31f532ea4e618caf53
Reilly Miller <reillyjmiller31@gmail.com>
2023-06-12 15:48:19 -0600

    add create action in users controller

---
+++ app/controllers/api/v0/users_controller.rb
@@ -0,0 +1,17 @@
+class Api::V0::UsersController < ApplicationController
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: UsersSerializer.new(user), status: 201
+    else
+      render json: {error: user.errors.full_messages.to_sentence}, status: 400
+    end
+  end
+
+  private
+
+    def user_params
+      params.permit(:email, :password, :password_confirmation)
+    end
+end
