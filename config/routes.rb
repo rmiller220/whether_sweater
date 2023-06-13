695c8d28941584fee1edea0f8fd8db6c721e1e8c
Reilly Miller <reillyjmiller31@gmail.com>
2023-06-13 13:31:53 -0600

    add roadtrip to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,12 @@
 Rails.application.routes.draw do
-    # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-    # Defines the root path route ("/")
-    # root "articles#index"
   namespace :api do
     namespace :v0 do
       resources :forecast, only: [:index]
       post :users, to: 'users#create'
       post :sessions, to: 'sessions#login'
+      post :road_trip, to: 'roadtrip#create'
       get '/book-search', to: 'book_search#index'
-
     end
   end
 end
